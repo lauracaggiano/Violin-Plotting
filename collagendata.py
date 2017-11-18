bb0f4106be60562bcd7cc0cb7cb6730d31993391
fluviolobo <fluvio.lobo@gmail.com>
2017-11-18 14:38:37 -0500

    Finished a pyhton code that can plot the datasets as originally planned.

--- collagendata.py
+++ collagendata.py
@@ -13,59 +13,156 @@ import seaborn as sns
 import pandas as pd
 
 
-# Loading data
+"""
+Loading Data
+
+I created several sheets to see if we can somehow get closer to what you want
+to represent, show or plot
+"""
 x1 = pd.ExcelFile('HistologyDataPythonNorm.xlsx')
 df1 = x1.parse('Sheet1')
+df2 = x1.parse('Sheet2')                                             # Test sheet... used to show that the graph does not pull a 3rd array
+dfw1 = x1.parse('Week1')                                             # As I will try to explain later, this may be the only way of showing your Angle vs. Norm Coll plot
+dfw2 = x1.parse('Week2')
+dfw3 = x1.parse('Week3')
 
-Week1AvgNP = df1.loc[(df1['Patch']=='N') & (df1['Week'] == 1)]
-Week1AvgP = df1.loc[(df1['Patch']=='Y') & (df1['Week'] == 1)]
+df1_array = df1.as_matrix(columns=None)                              # Numpy array conversion of the dataframe
+dfw1_array = dfw1.as_matrix(columns=None)
+dfw2_array = dfw2.as_matrix(columns=None)
+dfw3_array = dfw3.as_matrix(columns=None)
 
-Week2AvgNP = df1.loc[(df1['Patch']=='N') & (df1['Week'] == 2)]
-Week2AvgP = df1.loc[(df1['Patch']=='Y') & (df1['Week'] == 2)]
+"""
+Data Extraction
 
-Week3AvgNP = df1.loc[(df1['Patch']=='N') & (df1['Week'] == 3)]
-Week3AvgP = df1.loc[(df1['Patch']=='Y') & (df1['Week'] == 3)]
+This is the same classification that you built, I am just using a different
+tool to plot it
+"""
+
+w1np = df1.loc[(df1['Patch']=='N') & (df1['Week'] == 1)]
+w1np_array = w1np.as_matrix(columns=None)
+w1p = df1.loc[(df1['Patch']=='Y') & (df1['Week'] == 1)]
+w1p_array = w1p.as_matrix(columns=None)
+
+w2np = df1.loc[(df1['Patch']=='N') & (df1['Week'] == 2)]
+w2np_array = w2np.as_matrix(columns=None)
+w2p = df1.loc[(df1['Patch']=='Y') & (df1['Week'] == 2)]
+w2p_array = w2p.as_matrix(columns=None)
+
+w3np = df1.loc[(df1['Patch']=='N') & (df1['Week'] == 3)]
+w3np_array = w3np.as_matrix(columns=None)
+w3p = df1.loc[(df1['Patch']=='Y') & (df1['Week'] == 3)]
+w3p_array = w3p.as_matrix(columns=None)
+
+"""
+Simple Plot
+
+Same plotting you did, but goruped
+"""
+# Week 1
+plt.figure()
+plt.plot(w1np_array[:,0],w1np_array[:,1], "b.")
+plt.plot(w1p_array[:,0],w1p_array[:,1], "r.")
+plt.xlabel("Angle")
+plt.ylabel("Norm Coll")
+plt.title("Week 1")
+plt.legend({"Y","N"})                                                # Please check that these actually match the with/without patch groups - the array had to be inserted in a weird order
+plt.show()
+
+# Week 2
+plt.figure()
+plt.plot(w2np_array[:,0],w2np_array[:,1], "b.")
+plt.plot(w2p_array[:,0],w2p_array[:,1], "r.")
+plt.xlabel("Angle")
+plt.ylabel("Norm Coll")
+plt.title("Week 2")
+plt.legend({"Y","N"})                                                # Please check that these actually match the with/without patch groups - the array had to be inserted in a weird order
+plt.show()
+
+# Week3
+plt.figure()
+plt.plot(w3np_array[:,0],w3np_array[:,1], "b.")
+plt.plot(w3p_array[:,0],w3p_array[:,1], "r.")
+plt.xlabel("Angle")
+plt.ylabel("Norm Coll")
+plt.title("Week 3")
+plt.legend({"Y","N"})                                                # Please check that these actually match the with/without patch groups - the array had to be inserted in a weird order
+plt.show()
 
 """
 Histograms
 
-- SNS also has some distribution plots that you may want to look at.
-  These helped me understand what was going on
+SNS also has some distribution plots that you may want to look at.
+These helped me understand what was going on
 """
 
+# Week1 Histogram - Norm Coll (only)
+plt.figure()
+sns.distplot(w1np_array[:,1])
+sns.distplot(w1p_array[:,1])
+plt.title("Week 1 - Norm Coll")
+plt.xlabel("Norm Coll")
 
+# Week2 Histogram - Norm Coll (only)
+plt.figure()
+sns.distplot(w2np_array[:,1])
+sns.distplot(w2p_array[:,1])
+plt.title("Week 2 - Norm Coll")
+plt.xlabel("Norm Coll")
 
-#plt.plot(Week1AvgNP['Angle'], Week1AvgNP['Norm Coll'])
-#plt.figure()
-#plt.plot(Week1AvgP['Angle'], Week1AvgP['Norm Coll'])
-#plt.figure()
-#plt.plot(Week2AvgNP['Angle'], Week2AvgNP['Norm Coll'])
-#plt.figure()
-#plt.plot(Week2AvgP['Angle'], Week2AvgP['Norm Coll'])
-#plt.figure()
-#plt.plot(Week3AvgNP['Angle'], Week3AvgNP['Norm Coll'])
-#plt.figure()
-#plt.plot(Week3AvgP['Angle'], Week3AvgP['Norm Coll'])
+# Week3 Histogram - Norm Coll (only)
+plt.figure()
+sns.distplot(w3np_array[:,1])
+sns.distplot(w3p_array[:,1])
+plt.title("Week 3 - Norm Coll")
+plt.xlabel("Norm Coll")
 
 
 """
 Boxplot
+
+SNS also has box-plots, which are essentially a segment of the violin plot
 """
 
-# Draw a nested boxplot to show bills by day and sex
+# The whole DataSet
 plt.figure()
 sns.boxplot(x="Week", y="NormColl", hue="Patch", data=df1, palette="PRGn")
 sns.despine(offset=10, trim=True)
 
+# WARNING: BE CAREFUL WHEN UNCOMMENTING THIS SECTION
+# Note what happens when you try plotting the Angle vs. Norm Coll
+# For this plot, I use the data from a single week (otherwise the computer explodes)
+# You would also have to change the tickmarks for the x axis
+
+#plt.figure()
+#ax = sns.boxplot(x="Angle", y="NormColl", hue="Patch", data=df1, palette="PRGn")
+#sns.despine(offset=10, trim=True)
+
+# Trying to plot this made me realize that your collagen normals is already distributed to the angles
+# Therefore, I think the only thing you need to do is...
 
 """ 
-This is my attempt at a violin plot
-You can see it's not plotting the data that I want to show
-The x axis should be weeks, the y axis should be angle
-The magnitude of the histogram should correspond to Norm Coll
+ViolinPlot
+
+Here is how I think yout violinplot should look like. Technically, your
+Collagen Normals dataset has been distributed to the orientation angles.
+Look at the similarities between the histogram plots, the boxplot and the
+violin plot.
+
+Another hint here was that the violint plot function only takes two dataset,
+not three (as you would assume). Essentially, it is expected that the collagen
+normal is already a distribution of another controllable variable (in this
+case, the angles) - I am like 75% sure hahahah
+
 """
 
+# This is the plot that you want :)))
 plt.figure()
 ax = sns.violinplot(x = df1["Week"], y = df1['NormColl'], hue = "Patch", data = df1, palette = "muted", scale="count")
+# I do not know how to interpret it, but I am pretty sure you can explain it to me hahaha
+
 plt.figure()
-ax = sns.violinplot(x = df1["Angle"], y = df1['NormColl'], hue = "Patch", data = df1, palette = "muted", scale="count")
+ax = sns.violinplot(x = df1["Week"], y = df1['NormColl'], hue = "Patch", data = df1, palette = "muted", scale="count", inner="quart")
+
+# WARNING: BE CAREFUL WHEN UNCOMMENTING THIS SECTION
+#plt.figure()
+#ax = sns.violinplot(x = df1["Angle"], y = df1['NormColl'], hue = "Patch", data = df1, palette = "muted", scale="count")
